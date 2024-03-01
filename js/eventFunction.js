4691d244b2aa7e02644056ffbc02cfaf92842c85
yanglanping <2795990659@qq.com>
2024-03-01 17:28:41 +0800

    完善内容

--- js/eventFunction.js
+++ js/eventFunction.js
@@ -2,15 +2,15 @@
 const parentWindow = window.parent;
 window.addEventListener('wheel', function(e) {
   // 发送消息到父页面
-  parentWindow.postMessage({type:'wheel',delta:e.deltaY}, '*');
+  parentWindow.postMessage({type:'wheel',data:e.deltaY}, '*');
 },{ passive: false })
 
 window.addEventListener('keyup',function(event){
   // 发送消息到父页面
-  parentWindow.postMessage({type:'keyup',keyCode:event.keyCode}, '*');
+  parentWindow.postMessage({type:'keyup',data:event.keyCode}, '*');
 })
 
 // 禁止鼠标右键
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
-})
+})  
